5925a807bdb70d92d34811e8a781a2f81b3a1c52
Lea Rosema <terabaud@gmail.com>
2019-12-02 22:23:19 +0100

    Updates

--- bundle.js
+++ bundle.js
@@ -23,8 +23,6 @@ function testMediaQuery(query) {
 }
 setSize();
 onresize = setSize;
-var isDarkMode = testMediaQuery('(prefers-color-scheme: dark)');
-var prefersReducedMotion = testMediaQuery('(prefers-reduced-motion: reduce)');
 var Particle = /** @class */ (function () {
     function Particle() {
         this.x = (random() * w) | 0;
@@ -33,18 +31,32 @@ var Particle = /** @class */ (function () {
         this.w = (random() * (5 + w)) | 0;
         this.h = (random() * (5 + h)) | 0;
     }
+    Object.defineProperty(Particle.prototype, "isDarkMode", {
+        get: function () {
+            return testMediaQuery('(prefers-color-scheme: dark)');
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Particle.prototype, "prefersReducedMotion", {
+        get: function () {
+            return testMediaQuery('(prefers-reduced-motion: reduce)');
+        },
+        enumerable: true,
+        configurable: true
+    });
     Particle.prototype.render = function () {
         c.save();
         c.translate(this.x, this.y);
         c.rotate(this.r * DEG);
         c.translate(-this.x, -this.y);
-        c.fillStyle = isDarkMode ? 'rgba(0, 0, 0, .1)' : 'rgba(255,255,255,.1)';
+        c.fillStyle = this.isDarkMode ? 'rgba(0, 0, 0, .1)' : 'rgba(255,255,255,.1)';
         c.fillRect(this.x - this.w / 2, this.x - this.w / 2, this.w, this.h);
         c.restore();
     };
     Particle.prototype.move = function () {
-        if (!prefersReducedMotion) {
-            this.r = (this.r + .01) % 360;
+        if (!this.prefersReducedMotion) {
+            this.r = (this.r + .05) % 360;
         }
     };
     return Particle;
@@ -58,6 +70,8 @@ function animation(frame) {
         particles[i].render();
         particles[i].move();
     }
-    requestAnimationFrame(animation);
+    window.setTimeout(function () {
+        requestAnimationFrame(animation);
+    }, 33);
 }
 animation(0);
